0e637686a3ab4658535afdbf91f2f7e67a5b5ce2
Vincent Emonet <vincent.emonet@gmail.com>
2023-03-16 13:59:04 +0100

    docs

--- docs/fix_docs.sh
+++ docs/fix_docs.sh
@@ -4,7 +4,12 @@ find ./docs-dist/ -type f -exec sed -i "s/<li .*>Contributing<.*li>//g" {} +
 
 # Import nanopub-display.js
 cp dist/nanopub-display.min.js docs-dist/
-find ./docs-dist/ -type f -exec sed -i 's/display"\/><meta name="viewport/display"\/><script type="module" src="\/nanopub-display.min.js"><\/script><link rel="icon" href="https:\/\/raw.githubusercontent.com\/Nanopublication\/nanopub-website\/9fee248bac306dcff7b1b0fb3d645ef6d5b62ba7\/static\/img\/icon.png"\/><meta name="viewport/g' {} +
+# Fix issue with local dev
+mkdir -p docs-dist/nanopub-display/
+cp dist/nanopub-display.min.js docs-dist/nanopub-display/
+
+
+find ./docs-dist/ -type f -exec sed -i 's/display"\/><meta name="viewport/display"\/><script type="module" src="\/nanopub-display\/nanopub-display.min.js"><\/script><link rel="icon" href="https:\/\/raw.githubusercontent.com\/Nanopublication\/nanopub-website\/9fee248bac306dcff7b1b0fb3d645ef6d5b62ba7\/static\/img\/icon.png"\/><meta name="viewport/g' {} +
 
 # find ./docs-dist/ -type f -exec sed -i 's/display"\/><meta name="viewport/display"\/><script type="module" src="https:\/\/cdn.jsdelivr.net\/npm\/@nanopub\/display\/dist\/nanopub-display.bundle.js"><\/script><link rel="icon" href="https:\/\/raw.githubusercontent.com\/Nanopublication\/nanopub-website\/9fee248bac306dcff7b1b0fb3d645ef6d5b62ba7\/static\/img\/icon.png"\/><meta name="viewport/g' {} +
 # find ./docs-dist/ -type f -exec sed -i 's/display"\/><meta name="viewport/display"\/><script type="module" src="https:\/\/cdn.jsdelivr.net\/npm\/@nanopub\/display@0.0.4\/dist\/nanopub-display.min.js"><\/script><link rel="icon" href="https:\/\/raw.githubusercontent.com\/Nanopublication\/nanopub-website\/9fee248bac306dcff7b1b0fb3d645ef6d5b62ba7\/static\/img\/icon.png"\/><meta name="viewport/g' {} +
